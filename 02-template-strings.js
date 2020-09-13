beb2e27620d6df383d5ddfe3da0d6407ad3250a3
David Massana <00072416@uca.edu.sv>
2020-09-13 16:07:08 -0600

    Add new file: template-strings

---
+++ 02-template-strings.js
@@ -0,0 +1,10 @@
+const nombre = 'David';
+const apellido = 'Massana';
+
+console.log(nombre, apellido);
+
+//TEMPLATE STRINGS
+
+console.log(
+	`Hola, mi nombre es: ${nombre} ${apellido} y mi edad es: ${20 + 2} años`
+);
