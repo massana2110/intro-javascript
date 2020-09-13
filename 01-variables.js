a9628213a2e76b75332ae81cab4450ace282ce04
David Massana <00072416@uca.edu.sv>
2020-09-13 15:24:36 -0600

    First commit

---
+++ 01-variables.js
@@ -0,0 +1,17 @@
+console.log('Hola mundo');
+console.log('Esto es otro console log');
+
+// Variables.
+
+const nombre = 'David'; //a una variable const no se le puede cambiar el valor
+let apellido = 'Massana'; // a una let si se le puede cambiar
+
+console.log(nombre); //imprimis datos en consola
+console.log(typeof apellido); //typeof regresa el tipo de dato contenido en la variable.
+
+apellido = 'Zepeda';
+
+// nombre = 'Jose'; ESTO NO SE PUEDE HACER PORQUE ES CONST
+let edad = 19;
+
+console.log(edad);
