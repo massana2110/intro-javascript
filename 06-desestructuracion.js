8c7cf172d6730f97ac1f99ab80af700adfc8b019
David Massana <00072416@uca.edu.sv>
2020-09-14 18:52:36 -0600

    Objects desestructuring

---
+++ 06-desestructuracion.js
@@ -0,0 +1,35 @@
+// Desestructuracion
+
+const persona = {
+	nombre: 'David',
+	edad: 22,
+	clave: 'DAMZ1021',
+};
+
+const { nombre, edad, clave } = persona; //selecciona las propiedades del objeto
+
+console.log(nombre);
+console.log(edad);
+console.log(clave);
+
+const useContext = ({ nombre, clave, edad, rango = 'Capitan' }) => {
+	// console.log(nombre, edad, rango);
+	return {
+		nombreClave: clave,
+		anios: edad,
+		latlng: {
+			lat: 124.145,
+			lng: -12.351,
+		},
+	};
+};
+
+const {
+	nombreClave,
+	anios,
+	latlng: { lat, lng },
+} = useContext(persona);
+
+// const { nombreClave, anios } = avenger;
+
+console.log(nombreClave, anios, lat, lng);
