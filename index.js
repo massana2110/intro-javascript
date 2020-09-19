c9383f615c0553cf67a5bdfa32ed463346eb3bd7
David Massana <00072416@uca.edu.sv>
2020-09-18 20:37:49 -0600

    Add excercises

---
+++ index.js
@@ -0,0 +1,9 @@
+// Realice una función que reciba un arreglo como parámetro y liste los tipos de datos de sus elementos y cuantos de cada tipo.
+
+// Realice una función que reciba un arreglo de datos numéricos, y muestre la suma de todos los elementos del arreglo y su promedio.
+
+// Realice una función en JS, que dados un número y un arreglo como parámetros, retorne cuantas ocurrencias existen de ese número dentro del arreglo.
+
+// Elabore una función que llene un arreglo con 20 números de forma aleatoria (random), y que pida al usuario adivinar un número, si lo adivina mostrará un mensaje indicando que ganó, si no mostrará que perdió.
+
+// Análisis de una circunferencia: Crear una función en JavaScript que pida por el teclado el radio de un circunferencia, y calcule el área de esta y lo retorne, si el radio ingresado es negativo o cero, retornará -1.
